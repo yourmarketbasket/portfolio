c2bd8709831719bad2fbe2860185b2db4f1e0ddf
codethelabs <codethelabs@gmail.com>
2025-06-07 10:32:12 +0300

    added some pages

---
+++ src/app/pages/downloads/downloads.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-downloads',
+  standalone: true,
+  imports: [],
+  templateUrl: './downloads.component.html',
+  styleUrl: './downloads.component.scss'
+})
+export class DownloadsComponent {
+
+}
