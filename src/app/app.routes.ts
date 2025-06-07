83423d0351ce2883622ae20686267d12ce4ddf31
codethelabs <codethelabs@gmail.com>
2025-06-07 14:17:13 +0300

    added some pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LandingComponent } from '../pages/landing/landing.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent }
